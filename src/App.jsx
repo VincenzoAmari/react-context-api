f998308d92d395d2c930dcdb4da18dc8447a063c
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-04 18:02:52 +0100

    avvolgo l app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalContext"; 
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
-import PostDetail from "./pages/PostDetail"; 
-
+import PostDetail from "./pages/PostDetail";
+import Layout from "./components/Layout"; 
 function App() {
+
+  
   return (
-    <Router>
-      <nav>
-        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-        <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>Chi siamo</NavLink>
-        <NavLink to="/posts" className={({ isActive }) => (isActive ? "active" : "")}>Post</NavLink>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/posts/:id" element={<PostDetail />} /> {/* Aggiungiamo la pagina di dettaglio */}
-      </Routes>
-    </Router>
+    <GlobalProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </GlobalProvider>
   );
 }
 
